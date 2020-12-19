e9b64b302bc83de84b5bfbfe34eb040d5eee5d96
Vinay <vnchoudhary357@gmail.com>
2020-12-19 21:14:49 +0530

    updated webapp components

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
 @echo off
 pip install torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio===0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
+cmd /k
--- main.py
+++ main.py
@@ -116,4 +116,10 @@ def verify():
         print(e)
 
 
-main()
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
